9bcdb4e813a63735a075fc57345c83644e36e242
liangcongcongcc <tt_liangcongcong@163.com>
2020-06-04 08:45:47 +0800

    这是

---
+++ demo/src/com/cong/student.java
@@ -0,0 +1,22 @@
+package com.cong;
+
+public class student {
+    String name="cong";
+    int age=20;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
